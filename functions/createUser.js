791390da75008419d3568e22150ec3e3da146245
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-06-28 05:27:11 -0300

    adminId no user

--- functions/createUser.js
+++ functions/createUser.js
@@ -4,9 +4,9 @@ const { db, updateUsersDatabase } = require("../database");
 const salt = bcrypt.genSaltSync(10);
 
 function createUser(req, res) {
-  const { fullname, username, password, type } = req.body;
+  const { fullname, username, password, type, adminId } = req.body;
   const cryptPassword = bcrypt.hashSync(password, salt);
-  const user = { fullname, username, type, password: cryptPassword };
+  const user = { fullname, username, type, adminId, password: cryptPassword };
 
   updateUsersDatabase(user, ({ password, ...newUser }) => {
     res.send(newUser);
