be09baec7752953f9965e268d58fb5e4d6beea33
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-07-06 09:29:01 -0300

    commit

--- middlewares/checkAuthentication.js
+++ middlewares/checkAuthentication.js
@@ -5,8 +5,6 @@ function checkAuthentication(req, res, next) {
     res.status(400);
     res.send({ message: "User not authorized" });
   } else {
-    // const username = token.split(":")[1];
-
     next();
   }
 }
